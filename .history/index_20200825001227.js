58805b9c1dc10dc8153b11f16f948e8c6ccae9f7
whattosay <porwal.deepak2000@gmail.com>
2020-11-08 17:22:33 +0530

    Inital Commit

---
+++ .history/index_20200825001227.js
@@ -0,0 +1,40 @@
+var numberOfDrunButtons = document.querySelectorAll(".drum").length;
+
+for ( var i=0; i < numberOfDrunButtons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click", function()){
+        var buttonInnerHTML = this.buttonInnerHTML;
+
+        switch (buttonInnerHTML) {
+            case 'w':
+                var tom1 = new Audio("sounds/tom-1.mp3");
+                tom1.play();
+                break;
+            case 'a':
+                var tom1 = new Audio("sounds/tom-2.mp3");
+                tom1.play();
+                break;
+            case 's':
+                var tom1 = new Audio("sounds/tom-3.mp3");
+                tom1.play();
+                break;  
+            case 'd':
+                var tom1 = new Audio("sounds/tom-4.mp3");
+                tom1.play();
+                break;              
+            case 'j':
+                var tom1 = new Audio("sounds/share.mp3");
+                tom1.play();
+                break;
+            case 'k':
+                var tom1 = new Audio("sounds/crash.mp3");
+                tom1.play();
+                break;
+            case 'l':
+                var tom1 = new Audio("sounds/kick-bass.mp3");
+                tom1.play();
+                break;
+            default: console.log(buttonInnerHTML);
+                
+        }
+    }
+}
